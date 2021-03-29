62aede25aa3dac0b2f44db10f0198bbf9cdb0ce8
Gustavo Ribeiro <pokegustavo@gmail.com>
2021-03-29 16:40:23 -0300

    Adicionar arquivos de projeto.

---
+++ Sector numbers/Patch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace Sector_numbers
+{
+    [HarmonyPatch(typeof(PLGalaxy), "Update")]
+    class Patch
+    {
+        static void Postfix(PLGalaxy __instance)
+        {
+            foreach (PLSectorInfo sector in __instance.AllSectorInfos.Values)
+            {
+                int test;
+                bool vai = false;
+                if (int.TryParse(sector.Name, out test))
+                {
+                    vai = true;
+                }
+                if (!vai && !sector.Name.Contains("("))
+                {
+                    sector.Name = sector.Name + " (" + sector.ID + ")";
+                }
+            }
+        }
+    }
+}
---
+++ Sector numbers/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PulsarPluginLoader;
+
+namespace Sector_numbers
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "1.0";
+
+        public override string Author => "pokegustavo ";
+
+        public override string ShortDescription => "Makes all sectors have the ID number";
+
+        public override string Name => "Sector Numbers";
+
+        public override string HarmonyIdentifier()
+        {
+            return "pokegustavo.Sector_Numbers";
+        }
+    }
+}
